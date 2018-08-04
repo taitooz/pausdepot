be7c1f523ff2d1c4296c964c433aa073aed4bbb0
Emiliano Lourbet <elourbet@despegar.com>
2018-08-04 11:53:24 -0300

    remove unused code

--- src/main/java/com/pausa/stockservice/config/StockServiceApplication.java
+++ src/main/java/com/pausa/stockservice/config/StockServiceApplication.java
@@ -52,22 +52,4 @@ public class StockServiceApplication {
         resolver.setSuffix(".html");
         return resolver;
     }
-
-
-//    @Bean
-//    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
-//        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
-//        registration.addUrlMappings("/*");
-//        return registration;
-//    }
-
-//    @Bean
-//    public EmbeddedServletContainerCustomizer containerCustomizer() {
-//        return container -> {
-//            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/static/401.html");
-//            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/static/404.html");
-//            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/static/500.html");
-//            container.addErrorPages(error401Page, error404Page, error500Page);
-//        };
-//    }
 }
